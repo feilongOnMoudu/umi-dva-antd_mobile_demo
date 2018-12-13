24e652731285054bafcdf00ed538779b5990cd1b
songfeilong <songfeilong@cifm.com>
2018-12-13 16:12:50 +0800

    666

--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -1,15 +1,26 @@
 import {NavBar,Icon} from 'antd-mobile';
 import Link from 'umi/link';
+import PropTypes from 'prop-types';
 
-function Nav ({location}) {
+function Nav (props) {
     return (
         <NavBar
         mode="light"
         icon={<Icon type="left" />}
-        onLeftClick={() => console.log('onLeftClick')}
-      >NavBar</NavBar>
+        onLeftClick={!props.showLeftButton ? null : props.onLeftClick}
+      >{props.navTitile}</NavBar>
     );
 }
 
-
 export default Nav;
+
+Nav.defaultProps = {
+    showLeftButton: false,
+    navTitile:''
+  };
+  
+  Nav.propTypes = {
+    showLeftButton: PropTypes.bool,
+    navTitile:  PropTypes.string,
+    onLeftClick: PropTypes.func.isRequired
+  };
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,13 +2,23 @@ import { Button, TabBar } from 'antd-mobile';
 import { Component } from 'react';
 import { connect } from 'dva';
 import Nav from '../../layouts/Nav';
+import PropTypes from 'prop-types';
 
-function Home () {
-  return (
-    <Nav>
-      
-    </Nav>
-  );
+class Home extends Component {
+  render () {
+    const { showLeftButton,navTitile } = this.props;
+    function onLeftClick () {
+      console.log(1111);
+    }
+
+    return (
+        <Nav onLeftClick={onLeftClick} showLeftButton={true} navTitile='111'>
+
+        </Nav>
+    )
+  }
 }
 
 export default connect()(Home);
+
+
